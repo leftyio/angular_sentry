1b052fee6dcf960a07a745ad7e03dbda29a68a83
hadrien <hadrien.lejard@gmail.com>
2018-03-06 12:11:08 +0100

    use sentry package instead of sentry_client

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.0.2
+
+- Use `sentry` package instead of `sentry_client`
+
 ## 0.0.1
 
 - Initial version, created by Stagehand
--- lib/angular_sentry.dart
+++ lib/angular_sentry.dart
@@ -1,35 +1,32 @@
 library angular_sentry;
 
+import "dart:html" as html;
 import 'dart:async';
 
 import 'package:angular/angular.dart';
 import 'package:logging/logging.dart';
-import 'package:sentry_client/api_data/sentry_exception.dart';
-import 'package:sentry_client/api_data/sentry_packet.dart';
-import 'package:sentry_client/api_data/sentry_stacktrace.dart';
-import 'package:sentry_client/api_data/sentry_stacktrace_frame.dart';
-import 'package:sentry_client/api_data/sentry_user.dart';
-import 'package:sentry_client/sentry_client_browser.dart';
-import 'package:sentry_client/sentry_dsn.dart';
+import "package:sentry/sentry.dart";
 import 'package:stack_trace/stack_trace.dart';
-
-export 'package:sentry_client/api_data/sentry_user.dart';
-export 'package:stack_trace/stack_trace.dart';
+import "package:http/browser_client.dart";
 
 const OpaqueToken SENTRY_DSN = const OpaqueToken('sentryDSN');
 
 @Injectable()
 class AngularSentry implements ExceptionHandler {
-  SentryClientBrowser _sentry;
+  SentryClient _sentry;
   Logger _log;
   Logger get log => _log;
   ApplicationRef _appRef;
 
   AngularSentry(
-      Injector injector, @Optional() @Inject(SENTRY_DSN) String sentryDSN) {
+      Injector injector,
+      @Optional() @Inject(SENTRY_DSN) String sentryDSN,
+      @Optional() BrowserClient client) {
     if (sentryDSN != null) {
-      _sentry = new SentryClientBrowser(
-          SentryDsn.fromString(sentryDSN, allowSecretKey: true));
+      _sentry = new SentryClient(
+          dsn: sentryDSN,
+          compressPayload: false,
+          httpClient: client ?? new BrowserClient());
     }
 
     _log = new Logger("$runtimeType");
@@ -41,7 +38,7 @@ class AngularSentry implements ExceptionHandler {
 
   void onCatch(dynamic exception, Trace trace, [String reason]) {
     try {
-      _send(exception, trace);
+      _send(exception, trace, reason);
     } catch (e, s) {
       logError(e, s);
       // do nothing;
@@ -69,8 +66,10 @@ class AngularSentry implements ExceptionHandler {
   /// provide environment data to the sentry report
   String get environment => null;
 
-  /// provide user data to the sentry report
-  SentryUser get user => null;
+  Map<String, String> get tags => {
+    "userAgent": html.window.navigator.userAgent,
+    "platform": html.window.navigator.platform
+  };
 
   /// The release version of the application.
   String get release => null;
@@ -78,30 +77,16 @@ class AngularSentry implements ExceptionHandler {
   /// provide extra data to the sentry report
   Map<String, String> get extra => null;
 
-  void _send(dynamic exception, Trace trace) {
-    final stacktraceValue = new SentryStacktrace(
-        frames: trace?.frames
-            ?.map((Frame frame) => new SentryStacktraceFrame(
-                filename: frame?.uri?.toString(),
-                package: frame?.package,
-                lineno: frame?.line,
-                colno: frame?.column,
-                function: frame?.member,
-                module: frame?.library))
-            ?.toList());
-
-    final exceptionValue = new SentryException(
-        type: exception?.runtimeType?.toString(),
-        value: exception?.toString(),
-        stacktrace: stacktraceValue);
-
-    final packet = new SentryPacket(
-        user: user,
-        environment: environment,
-        release: release,
-        exceptionValues: [exceptionValue],
-        extra: extra);
-    _sentry?.write(packet);
+  void _send(dynamic exception, dynamic stackTrace, String reason) {
+    _sentry?.capture(
+        event: new Event(
+            exception: exception,
+            stackTrace: stackTrace,
+            release: release,
+            environment: environment,
+            extra: extra,
+            tags: tags,
+            message: reason));
   }
 }
 
